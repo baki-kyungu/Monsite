eaadb5e00c19c88d1b01d8e3a0ec8190990ce7dc
fabuleux366 <abbakikyungu@gmail.com>
2023-02-27 12:14:30 -0500

    premère version livrable

---
+++ script.js
@@ -0,0 +1,16 @@
+const navMenu = document.getElementById ('nav__menu'),
+
+      navToggle = document.getElementById ('nav__toggle')
+
+
+      if (navToggle){
+
+        navToggle.addEventListener ('click', () =>{
+
+            navMenu.style.visibility = "visible";   
+ 
+        })
+
+
+      }
+      
