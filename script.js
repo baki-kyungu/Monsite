52ab12f5daaac9e8a5ed3399b4da7809ce0008f5
fabuleux366 <abbakikyungu@gmail.com>
2023-03-06 17:38:29 -0500

    changement de couleur de la nav après la revue  de code avec les amis

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
-const navMenu = document.getElementById ('nav__menu'),
 
-      navToggle = document.getElementById ('nav__toggle')
+const contactLink = document.querySelector('.button');
+const contactSection = document.querySelector('#contact');
 
+contactLink.addEventListener('click', function() {
+  // contactSection.classList.add('visible');
 
-      if (navToggle){
-
-        navToggle.addEventListener ('click', () =>{
-
-            navMenu.style.visibility = "visible";   
- 
-        })
-
-
-      }
-
-const contactMe = document.getElementsByClassName ('contactMe__section')
-
-
-// ==============================================Animation section=======================================================================
-
-document.addEventListener("DOMContentLoaded", function(event) {
-  //Removing article link when on mobiforge
-  console.log(document.referrer);
-  if (parent !== window && document.referrer.indexOf('https://mobiforge.com') === 0 && document.referrer.indexOf('http://mobiforge.com') === 0)
-  {  
-    console.log(document.referrer);
-    document.getElementById('article-link').className = "fade-out";
+  if (contact.style.visibility === "hidden" && contact.style.display === "none") {
+    contact.style.visibility = "visible";
+    contact.style.display = "flex";
+  } else {
+    contact.style.visibility = "hidden";
+    contact.style.display = "none";
   }
-
+  
 });
+
+
 
 // ==============================================Form================================================================================
 
