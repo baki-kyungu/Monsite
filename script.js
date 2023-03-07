b635f88fb665d9c2d753b48509ee88a09fd420e6
fabuleux366 <abbakikyungu@gmail.com>
2023-03-07 13:06:32 -0500

    nouvelle modif

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const contactLink = document.querySelector('.button');
 const contactSection = document.querySelector('#contact');
 
 contactLink.addEventListener('click', function() {
-  // contactSection.classList.add('visible');
 
   if (contact.style.visibility === "hidden" && contact.style.display === "none") {
     contact.style.visibility = "visible";
